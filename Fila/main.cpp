10f1e0448e16dc92b22f27492926d85c11a9e302
diogommartins <diogo.martins@unirio.br>
2015-03-23 18:15:30 -0300

    Initial Commit

---
+++ Fila/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fila
+//
+//  Created by Diogo Martins on 3/23/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
